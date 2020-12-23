9b1c3741837cb66e905a7901cb78e8a37e64e459
Sweta Sharma <sweta.sharma@siemens.com>
2020-12-23 18:58:22 +0530

    fixed The provider provider.aws does not support resource type aws_S3_bucket.

--- first_code.tf
+++ first_code.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_S3_bucket" "tf_s3_bucket"{
+resource "aws_s3_bucket" "tf_s3_bucket"{
   bucket = "tf-s3-bucket-23122020"
   acl = "private"
 }
